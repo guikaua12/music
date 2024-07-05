1e3c19ee337b10a4552ecfef4f1c0f47c2767703
Approximations <datkidbosst@gmail.com>
2024-07-05 20:41:51 -0300

    feat: Add Google login functionality to Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,30 @@
+'use client';
+
 import React from 'react';
 import { Container, HeaderWrapper } from '@/components/Header/Header.styles';
 import Button from '@/components/Button/Button';
 import { PiCloudArrowUpBold } from 'react-icons/pi';
 import SearchBar from '@/components/SearchBar/SearchBar';
+import { useAuth } from '@/hooks/useAuth';
+import GoogleLoginButton from '../LoginButton/GoogleLoginButton';
 
 const Header = () => {
+    const { userQuery } = useAuth();
+
     return (
         <HeaderWrapper>
             <Container>
                 <SearchBar>Procure por uma música</SearchBar>
-                <Button>
-                    <PiCloudArrowUpBold size={20} color="white" />
-                </Button>
+                {userQuery.data ? (
+                    <div>
+                        <Button>
+                            <PiCloudArrowUpBold size={20} color="white" />
+                        </Button>
+                        <div>{userQuery.data.name}</div>
+                    </div>
+                ) : (
+                    <GoogleLoginButton />
+                )}
             </Container>
         </HeaderWrapper>
     );
