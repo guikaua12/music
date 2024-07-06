392101f9a08df8336a93bc31f819defb432a52f1
Approximations <datkidbosst@gmail.com>
2024-07-06 17:31:23 -0300

    fix: Update UserService to return user data from API response

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -2,5 +2,6 @@ import { User } from '@/types/User';
 import { api } from './api';
 
 export async function userInfo(): Promise<User> {
-    return api.get('/user/me');
+    const response = await api.get('/user/me');
+    return response.data;
 }
