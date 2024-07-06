e3cdf6df17da269283d50d2368663420bd3f6d10
Approximations <datkidbosst@gmail.com>
2024-07-06 17:33:01 -0300

    refactor: Update FullscreenLoading component styles to use styled-components animation

--- src/components/FullscreenLoading/FullscreenLoading.styles.ts
+++ src/components/FullscreenLoading/FullscreenLoading.styles.ts
@@ -1,5 +1,4 @@
-'use client';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import { AiOutlineLoading3Quarters } from 'react-icons/ai';
 
 export const Container = styled.div`
@@ -10,15 +9,15 @@ export const Container = styled.div`
     align-items: center;
 `;
 
-export const Spinner = styled(AiOutlineLoading3Quarters)`
-    animation: rotate 1s linear infinite;
-
-    @keyframes rotate {
-        0% {
-            transform: rotate(0deg);
-        }
-        100% {
-            transform: rotate(360deg);
-        }
+export const rotate = keyframes`
+    0% {
+        transform: rotate(0deg);
     }
+    100% {
+        transform: rotate(360deg);
+    }
+`;
+
+export const Spinner = styled(AiOutlineLoading3Quarters)`
+    animation: ${rotate} 1s linear infinite;
 `;
